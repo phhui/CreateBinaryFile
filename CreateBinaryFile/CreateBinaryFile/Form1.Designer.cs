ced79a5ad0d2db2be658371b828b86edb930156b
phhui <phhui@qq.com>
2012-12-16 15:12:10 +0800

    Create  adn read binary file
    将文件存储为二进制格式
    及读取二进制格式文件

---
+++ CreateBinaryFile/CreateBinaryFile/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace CreateBinaryFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_filePath = new System.Windows.Forms.TextBox();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_read = new System.Windows.Forms.Button();
+            this.txt_readPath = new System.Windows.Forms.TextBox();
+            this.txt_value = new System.Windows.Forms.TextBox();
+            this.pic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_filePath
+            // 
+            this.txt_filePath.Location = new System.Drawing.Point(4, 7);
+            this.txt_filePath.Name = "txt_filePath";
+            this.txt_filePath.ReadOnly = true;
+            this.txt_filePath.Size = new System.Drawing.Size(267, 21);
+            this.txt_filePath.TabIndex = 1;
+            this.txt_filePath.Text = "点击这里选择文件";
+            this.txt_filePath.Click += new System.EventHandler(this.txt_filePath_Click);
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(277, 5);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(93, 23);
+            this.btn_create.TabIndex = 2;
+            this.btn_create.Text = "存储为二进制";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // btn_read
+            // 
+            this.btn_read.Location = new System.Drawing.Point(277, 35);
+            this.btn_read.Name = "btn_read";
+            this.btn_read.Size = new System.Drawing.Size(93, 23);
+            this.btn_read.TabIndex = 3;
+            this.btn_read.Text = "读取二进制";
+            this.btn_read.UseVisualStyleBackColor = true;
+            this.btn_read.Click += new System.EventHandler(this.btn_read_Click);
+            // 
+            // txt_readPath
+            // 
+            this.txt_readPath.Location = new System.Drawing.Point(6, 34);
+            this.txt_readPath.Name = "txt_readPath";
+            this.txt_readPath.ReadOnly = true;
+            this.txt_readPath.Size = new System.Drawing.Size(265, 21);
+            this.txt_readPath.TabIndex = 4;
+            this.txt_readPath.Text = "点这里选择读取的文件";
+            this.txt_readPath.Click += new System.EventHandler(this.txt_readPath_Click);
+            // 
+            // txt_value
+            // 
+            this.txt_value.Location = new System.Drawing.Point(6, 62);
+            this.txt_value.Multiline = true;
+            this.txt_value.Name = "txt_value";
+            this.txt_value.Size = new System.Drawing.Size(357, 134);
+            this.txt_value.TabIndex = 5;
+            // 
+            // pic
+            // 
+            this.pic.Location = new System.Drawing.Point(13, 203);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(350, 309);
+            this.pic.TabIndex = 6;
+            this.pic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 524);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.txt_value);
+            this.Controls.Add(this.txt_readPath);
+            this.Controls.Add(this.btn_read);
+            this.Controls.Add(this.btn_create);
+            this.Controls.Add(this.txt_filePath);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_filePath;
+        private System.Windows.Forms.Button btn_create;
+        private System.Windows.Forms.Button btn_read;
+        private System.Windows.Forms.TextBox txt_readPath;
+        private System.Windows.Forms.TextBox txt_value;
+        private System.Windows.Forms.PictureBox pic;
+    }
+}
+
---
+++ CreateBinaryFile/CreateBinaryFile/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CreateBinaryFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void changeFile(TextBox tb)
+        {
+            OpenFileDialog fb = new OpenFileDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                tb.Text = fb.FileName;
+            }
+        }
+        private void txt_filePath_Click(object sender, EventArgs e)
+        {
+            changeFile(txt_filePath);
+        }
+
+        private void txt_readPath_Click(object sender, EventArgs e)
+        {
+            changeFile(txt_readPath);
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            string fileNameTime = Directory.GetCurrentDirectory() + "\\" + DateTime.Now.ToString("yyyyMMddhhmmss") + ".bitt";//获取当前程序路径并以当前系统时间为文件名创建文本文件
+            FileStream myStream = new FileStream(fileNameTime, FileMode.Create);//创建文件
+            BinaryWriter myWriter = new BinaryWriter(myStream);
+            byte a = 0xff;
+            myWriter.Write(File.ReadAllBytes(txt_filePath.Text));//向文件写入二进制值
+            myWriter.Close();
+            myStream.Close();
+        }
+
+        private void btn_read_Click(object sender, EventArgs e)
+        {
+            using (StreamReader sr = File.OpenText(txt_readPath.Text))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    txt_value.Text = s;
+                }
+                pic.Image = Image.FromStream(sr.BaseStream);
+            }
+        }
+    }
+}
